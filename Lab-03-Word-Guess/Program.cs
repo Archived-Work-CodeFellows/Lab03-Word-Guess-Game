e355c8ecbe7905e63320f9286a0d8cee437a9097
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-08 09:51:53 -0700

    Added CRUD operations and Unit Tests for file manipulation

--- Lab-03-Word-Guess/Program.cs
+++ Lab-03-Word-Guess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab_03_Word_Guess
 {
@@ -6,7 +7,64 @@ namespace Lab_03_Word_Guess
     {
         static void Main(string[] args)
         {
+            string path = "../../../testFile.txt";
             Console.WriteLine("Hello World!");
+            CreateFile(path);
+            ReadFile(path);
+            UpdateFile(path, "doggo");
+            ReadFile(path);
+            DeleteFile(path);
+        }
+
+        public static bool CreateFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.Write("This is a line of text");
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static bool ReadFile(string path)
+        {
+            try
+            {
+                string[] myText = File.ReadAllLines(path);
+                foreach(string element in myText)
+                {
+                    Console.WriteLine(element);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Oops...something went wrong!");
+            }
+            return false;
+        }
+
+        public static bool UpdateFile(string path, string word)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine("");
+                    sw.WriteLine(word);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static bool DeleteFile(string path)
+        {
+            File.Delete(path);
+            return true;
         }
     }
 }
