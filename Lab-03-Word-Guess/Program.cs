e02b7cc3e940aa8fc1805a5e4ec327cbf465e6a7
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-08 09:18:48 -0700

    Created project file and tests file

---
+++ Lab-03-Word-Guess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_03_Word_Guess
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Word-Guess-Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Lab_03_Word_Guess;
+using Xunit;
+
+namespace Word_Guess_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
